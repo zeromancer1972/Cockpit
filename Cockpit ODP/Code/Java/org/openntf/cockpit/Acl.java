22b2d678adfffb6393d146752e1d0cfe7b75a121
zeromancer1972 <obusse@googlemail.com>
2014-03-10 18:49:55 +0100

    First stage to an "application"

---
+++ Cockpit ODP/Code/Java/org/openntf/cockpit/Acl.java
@@ -0,0 +1,31 @@
+package org.openntf.cockpit;
+
+import java.io.Serializable;
+
+import lotus.domino.Database;
+import lotus.domino.NotesException;
+
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+
+public class Acl implements Serializable {
+
+	private static final long serialVersionUID = -1078384153186411254L;
+	private boolean createDocuments = false;
+
+	public Acl() {
+		try {
+			this.createDocuments = (ExtLibUtil.getCurrentDatabase().queryAccessPrivileges(ExtLibUtil.getCurrentSession().getEffectiveUserName()) & Database.DBACL_CREATE_DOCS) > 0;
+		} catch (NotesException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isCreateDocuments() {
+		return createDocuments;
+	}
+
+	public void setCreateDocuments(boolean createDocuments) {
+		this.createDocuments = createDocuments;
+	}
+}
---
+++ Cockpit ODP/Code/Java/org/openntf/cockpit/Metrics.java
@@ -0,0 +1,103 @@
+package org.openntf.cockpit;
+
+import java.io.Serializable;
+
+import lotus.domino.NotesException;
+
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+
+public class Metrics implements Serializable {
+
+	private static final long serialVersionUID = 1473132006122124544L;
+	private int ticketCount = 0;
+	private int urgentCount = 0;
+	private int solvedCount = 0;
+	private int unassignedCount = 0;
+	private int myCount = 0;
+	private float duration = 0;
+	private double quota = 0;
+
+	public Metrics() {
+		// calc all the values
+		try {
+			this.ticketCount = ExtLibUtil.getCurrentDatabase().getView("tickets").getAllEntries().getCount();
+			this.urgentCount = ExtLibUtil.getCurrentDatabase().search("SELECT ticketPriority=\"1\" & (ticketStatus!=\"90\" | ticketStatus!=\"99\")").getCount();
+			this.solvedCount = ExtLibUtil.getCurrentDatabase().search("SELECT ticketStatus=\"90\" | ticketStatus=\"99\"").getCount();
+			this.unassignedCount = ExtLibUtil.getCurrentDatabase().search("SELECT ticketStatus=\"\" | ticketResponsible=\"\"").getCount();
+			this.myCount = ExtLibUtil.getCurrentDatabase().getView("ticketsByResponsible").getAllEntriesByKey(ExtLibUtil.getCurrentSession().getEffectiveUserName()).getCount();
+			calcDuration();
+			calcQuota();
+		} catch (NotesException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void calcDuration() {
+
+	}
+
+	private void calcQuota() {
+		try {
+			this.quota = (this.solvedCount * 100.0f) / this.ticketCount;
+		} catch (Exception e) {
+
+		}
+	}
+
+	public int getMyCount() {
+		return myCount;
+	}
+
+	public void setMyCount(int myCount) {
+		this.myCount = myCount;
+	}
+
+	public int getTicketCount() {
+		return ticketCount;
+	}
+
+	public void setTicketCount(int ticketCount) {
+		this.ticketCount = ticketCount;
+	}
+
+	public int getUrgentCount() {
+		return urgentCount;
+	}
+
+	public void setUrgentCount(int urgentCount) {
+		this.urgentCount = urgentCount;
+	}
+
+	public int getSolvedCount() {
+		return solvedCount;
+	}
+
+	public void setSolvedCount(int solvedCount) {
+		this.solvedCount = solvedCount;
+	}
+
+	public int getUnassignedCount() {
+		return unassignedCount;
+	}
+
+	public void setUnassignedCount(int unassignedCount) {
+		this.unassignedCount = unassignedCount;
+	}
+
+	public float getDuration() {
+		return duration;
+	}
+
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+
+	public double getQuota() {
+		return quota;
+	}
+
+	public void setQuota(float quota) {
+		this.quota = quota;
+	}
+
+}
