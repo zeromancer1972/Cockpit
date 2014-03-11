9917239d4fd15926076f7c1fc45b9db8a28b88d5
zeromancer1972 <obusse@googlemail.com>
2014-03-11 21:40:56 +0100

    Layout, history, widgets, customer form

---
+++ Cockpit ODP/Code/Java/org/openntf/cockpit/History.java
@@ -0,0 +1,54 @@
+package org.openntf.cockpit;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Locale;
+
+import lotus.domino.Document;
+import lotus.domino.Item;
+import lotus.domino.Name;
+
+import com.ibm.icu.text.SimpleDateFormat;
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+
+public class History implements Serializable {
+
+	private static final long serialVersionUID = 1809269965845338393L;
+
+	public History() {
+
+	}
+
+	public void addHistory(Document doc, boolean isNew) {
+		try {
+			if (isNew) {
+				this.addHistory(doc, "QuickTicket");
+			} else {
+				this.addHistory(doc, "Ticket updated");
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
+	public void addHistory(Document doc, String msg) {
+		SimpleDateFormat formater = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss", Locale.ENGLISH);
+		String now = formater.format(new Date());
+		Name user = null;
+		Item it = null;
+		try {
+			user = ExtLibUtil.getCurrentSession().createName(ExtLibUtil.getCurrentSession().getEffectiveUserName());
+			msg = user.getAbbreviated() + "::" + now + "::" + msg;
+			if (doc.hasItem("ticketHistory")) {
+				it = doc.getFirstItem("ticketHistory");
+				it.appendToTextList(msg);
+			} else {
+				doc.replaceItemValue("ticketHistory", user.getAbbreviated() + "::" + now + "::Ticket created");
+			}
+			doc.save();
+			it.recycle();
+		} catch (Exception e) {
+
+		}
+	}
+}
--- Cockpit ODP/Code/ScriptLibraries/core.js
+++ Cockpit ODP/Code/ScriptLibraries/core.js
@@ -63,5 +63,23 @@ dojo.addOnLoad( function() {
 		$(this).select();
 	}).mouseup( function() {
 		return false;
-	});	
+	});
 })
+
+$(document).on(
+		'click',
+		'.panel-heading span.clickable',
+		function(e) {
+			var $this = $(this);
+			if (!$this.hasClass('panel-collapsed')) {
+				$this.parents('.panel').find('.panel-body').fadeOut();
+				$this.addClass('panel-collapsed');
+				$this.find('i').removeClass('glyphicon-chevron-up').addClass(
+						'glyphicon-chevron-down');
+			} else {
+				$this.parents('.panel').find('.panel-body').fadeIn();
+				$this.removeClass('panel-collapsed');
+				$this.find('i').removeClass('glyphicon-chevron-down').addClass(
+						'glyphicon-chevron-up');
+			}
+		});
